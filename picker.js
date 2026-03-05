a344e7d39771f4a4634448251f65e8c8ff2e423d
nate140 <nathangomes45@hotmail.fr>
2026-03-05 15:45:21 +0100

    Add files via upload

--- picker.js
+++ picker.js
@@ -172,6 +172,8 @@ function hardRestart() {
     clearProgress();
     location.reload();
 }
+
+function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
